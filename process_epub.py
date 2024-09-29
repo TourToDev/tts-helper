08984a579dc0f626e979d4ec3c0f1167d95beea6
Lawson <754386595@qq.com>
2024-09-29 15:38:06 +0800

    feat: epub processor output title optimization

--- process_epub.py
+++ process_epub.py
@@ -3,6 +3,7 @@ import argparse
 from ebooklib import epub
 from ebooklib import ITEM_DOCUMENT
 from bs4 import BeautifulSoup
+import re
 
 def epub_to_txt_chunks(epub_file, output_folder):
     book = epub.read_epub(epub_file)
@@ -11,21 +12,25 @@ def epub_to_txt_chunks(epub_file, output_folder):
 
     for item in book.get_items_of_type(ITEM_DOCUMENT):
         soup = BeautifulSoup(item.get_body_content(), 'html.parser')
-        text = soup.get_text()
-        output_file = os.path.join(output_folder, f'chapter_{chapter_count}.txt')
-        
-        with open(output_file, 'w', encoding='utf-8') as f:
-            f.write(text)
+        text = soup.get_text().strip()
 
-        print(f'Saved: {output_file}')
-        chapter_count += 1
+        if text:
+            title = text[:20]  # Take the first 15 characters of the content
+            title = re.sub(r'[<>:"/\\|?*]', '', title).strip()  # Remove invalid characters
+            title = title.replace('\n', '_').replace('\r', '_')
+            output_file = os.path.join(output_folder, f'{chapter_count}_{title}.txt')
+
+            with open(output_file, 'w', encoding='utf-8') as f:
+                f.write(text)
+
+            print(f'Saved: {output_file}')
+            chapter_count += 1
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Convert EPUB to TXT.')
     parser.add_argument('epub_file', type=str, help='Path to the EPUB file.')
     args = parser.parse_args()
 
-    # Create output folder based on EPUB file name
     epub_name = os.path.splitext(os.path.basename(args.epub_file))[0]
     output_folder = os.path.join('./output_txts', epub_name)
 
